284636508387284c6573f819c5f894a28c7ec2bb
酒井典昭 <sakainoriaki@sakainoriakinoMacBook-puro.local>
2019-09-22 21:26:21 +0900

    00017

---
+++ cp10-15/Pods/Target Support Files/Pods-cp10-15/Pods-cp10-15-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cp10_15 : NSObject
+@end
+@implementation PodsDummy_Pods_cp10_15
+@end
---
+++ cp10-16/Pods/Target Support Files/Pods-cp10-16/Pods-cp10-16-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cp10_16 : NSObject
+@end
+@implementation PodsDummy_Pods_cp10_16
+@end
